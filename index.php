eea7a8bb4f1d527776648594010029a5f6d51278
Gleizits <98473712+Gleizits@users.noreply.github.com>
2022-04-27 21:19:26 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>PHP Test</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <h1>This is a php test</h1>
+        <p>This is a PHP test how example.</p>
+    </body>
+</html>
